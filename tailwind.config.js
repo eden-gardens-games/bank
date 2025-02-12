98a671f3a3d3977a934bd683d7f2103dea84610f
Vineeth Gorla <winningvineeth@gmail.com>
2025-02-12 13:41:00 -0600

    sign in-color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+	  colors: {
+        'custom-blue': '#1E3A8A', // Replace with your preferred blue shade
+        'custom-gold': '#D4AF37', // Replace with your preferred gold shade
+        'custom-white': '#FFFFFF', // White
+      },
+	},
   },
   plugins: [],
 };
