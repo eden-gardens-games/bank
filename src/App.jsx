56954eeb5180799afda8b947937385f8bd7575d4
Vineeth Gorla <winningvineeth@gmail.com>
2025-02-13 23:04:31 -0600

    box sizing/routing1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Auth from "./components/Auth";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import AdminPage from "./components/AdminPage";
 import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/bank">
       <Routes>
-        <Route path="/bank" element={<Auth />} />
+        {/* You can render Auth at the root */}
+        <Route path="/" element={<Auth />} />
         <Route path="/bankAdmin" element={<AdminPage />} />
         <Route path="/bankDashboard" element={<Dashboard />} />
+        {/* Optional catch-all route */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
 }
+
 export default App;
