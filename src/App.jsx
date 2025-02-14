a6954618247105dba54d673de49564ca7a115309
Vineeth Gorla <winningvineeth@gmail.com>
2025-02-13 23:11:59 -0600

    box sizing/routing2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Auth from "./components/Auth";
 import AdminPage from "./components/AdminPage";
 import Dashboard from "./components/Dashboard";
