505f7bfcf52bfe3a92174c09ad7ad46e8e25d381
Vineeth Gorla <winningvineeth@gmail.com>
2025-02-17 14:47:05 -0600

    routing2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { ReactDOM } from 'react-dom/client'
 import './index.css'
 import { HashRouter as Router } from "react-router-dom"; 
 import App from './App.jsx'
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
